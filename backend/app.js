678f9b251cf09adbc047f09c959f0b66142391a4
Rimsha E. <111149954+rimshaejaz@users.noreply.github.com>
2024-10-05 17:05:35 -0700

    Add app.js, package-lock.json, package.json

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+// import dependencies 
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import routes from "./routes/routes.js";
+import mongoose from 'mongoose';
+
+
+const PORT = process.env.PORT;
+const app = express();
+
+// Middlewares
+app.use(express.json());  // REST needs JSON MIME type.
+app.use(cors());          // Help in accessing server 
+app.use("/", routes);
+
+app.listen(PORT,() => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+// Database connection
+mongoose 
+.connect(process.env.MONGODB_CONNECT_STRING)
+.then(() => console.log("Success: The database has been connected to MongoDB."))
+.catch((err)=> console.log("ERROR. DB NOT CONNECTED."))
