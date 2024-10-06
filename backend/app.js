01273921757385e55a546b7e5900acf45de8659b
Rimsha E. <111149954+rimshaejaz@users.noreply.github.com>
2024-10-06 11:36:08 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ mongoose
 // ------DEPLOYMENT---------
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1, '/frontend/build')));
+    app.use(express.static(path.join(__dirname1, '../frontend/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
     })
