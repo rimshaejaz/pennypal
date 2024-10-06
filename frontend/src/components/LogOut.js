5422948fa62fbf627d1a22f0453af719868192a1
Rimsha E. <111149954+rimshaejaz@users.noreply.github.com>
2024-10-06 13:54:58 -0700

    Update LogOut.js

--- frontend/src/components/LogOut.js
+++ frontend/src/components/LogOut.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+//import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+  //const { logout } = useAuth0();
+  const navigate = useNavigate();
+    const handleClick = () => {
+      navigate('/Welcome'); 
+    };
 
   return (
-    <button className='logoutButton' onClick={() => logout({ logoutParams: { returnTo: window.location.origin} })}>
-      Log Out
+    <button className='logoutButton' onClick={handleClick}>
+      EXIT
     </button>
   );
 };
