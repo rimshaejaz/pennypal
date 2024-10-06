21ad5ce16858e38c4fdeab22c99a84b46d880d13
Rimsha E. <111149954+rimshaejaz@users.noreply.github.com>
2024-10-06 13:54:40 -0700

    Update LogIn.js

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+//import { useAuth0 } from '@auth0/auth0-react'
+import { useNavigate } from 'react-router-dom';
 
 // handles login with auth0 redirect 
 
 const LogIn = () => {
-    const { loginWithRedirect } = useAuth0();
+    //const { loginWithRedirect } = useAuth0();
+    const navigate = useNavigate();
+    const handleClick = () => {
+      navigate('/Dashboard'); 
+    };
+
 
   return (
     <div className='center-button'>
-        <button className='loginButton' onClick={() => loginWithRedirect()}>
-            Log In
+        <button className='loginButton' onClick={handleClick}>
+            Get Started!
         </button>
     </div>
   );
