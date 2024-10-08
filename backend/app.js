c4e979dae59e1dbb09eead9f3625e2f3c7d34fc9
Rimsha E. <111149954+rimshaejaz@users.noreply.github.com>
2024-10-07 17:40:51 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from "./routes/routes.js";
 import mongoose from 'mongoose';
+import path from 'path';
 
 const PORT = process.env.PORT;
 const app = express();
@@ -23,3 +24,19 @@ mongoose
 .connect(process.env.MONGODB_CONNECT_STRING)
 .then(() => console.log("Success: The database has been connected to MongoDB."))
 .catch((err)=> console.log("ERROR. DB NOT CONNECTED."))
+
+
+// ------DEPLOYMENT---------
+const _dirname1 = path.resolve()
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(_dirname1, '../frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname1, "..", "frontend", "build", "index.html"))
+    })
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running successfully.")
+    });
+}
+// ------DEPLOYMENT---------
